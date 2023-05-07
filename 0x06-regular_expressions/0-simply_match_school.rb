179f50c576d830fb32c34a7ac33ad69aa9ee9d02
gebretewodros73 <gebretewodros73@gmail.com>
2023-05-07 05:14:05 +0300

    fits the output

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
 input_string = ARGV[0]
-regex = input_string.match(/School/, nil, Oniguruma::OPTION_NONE)
-if regex
-  puts regex[0]
+regex = input_string.scan(/School/)
+
+if regex.length > 0
+  puts regex.join
+
 else
   puts ""
 end
