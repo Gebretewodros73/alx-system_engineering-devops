ba59dc018816a63b023e93fbe1e526af768fefa5
gebretewodros73 <gebretewodros73@gmail.com>
2023-08-18 03:51:19 +0300

    OS configuration so that it is possible to login with the holberton user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,14 @@
-# Task 1: User Limit
+# Web Stack debugging: Increase limit of open files per user
 
-# Define an `exec` resource to modify the OS configuration for the holberton user
-exec { 'modify-os-configuration-for-holberton-user':
+# Define an `exec` resource to change the OS configuration for the holberton user
+exec { 'change-os-configuration-for-holberton-user':
   environment => ['DIR=/etc/security/limits.conf',  # Specify the directory of the limits.conf file
                   'OLD=hard nofile 5',              # Define the old limit setting for hard nofile
                   'NEW=hard nofile 50000',          # Define the new limit setting for hard nofile
                   'OLD2=soft nofile 4',             # Define the old limit setting for soft nofile
                   'NEW2=soft nofile 40000'],       # Define the new limit setting for soft nofile
-  command     => "sudo sed -i 's|$OLD|$NEW|' $DIR; sudo sed -i 's|$OLD2|$NEW2|' $DIR",  # Command to replace
+  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo sed -i "s/$OLD2/$NEW2/" $DIR',  # Command to replace
   path        => ['/usr/bin', '/bin'],             # Set the search path for the command execution
   returns     => [0, 1]                            # Specify the expected return codes (0: success, 1: no changes)
 }
+
