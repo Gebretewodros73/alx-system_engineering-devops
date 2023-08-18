28618f12a1eb7a1b3211805717c10d5dafd17057
gebretewodros73 <gebretewodros73@gmail.com>
2023-08-18 03:39:25 +0300

    optimize the Nginx web server setup to eliminate failed requests.

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,11 @@
-# File: nginx.conf.erb
+# Web Stack debugging: Increase limit of open files per user
 
-user www-data;
-worker_processes 4;  # Increase this value based on your server's resources
-error_log /var/log/nginx/error.log;
-pid /run/nginx.pid;
-
-events {
-  worker_connections 1024;  # Increase this value based on your server's resources
-}
-
-http {
-  # Other Nginx configuration settings...
+# Define an `exec` resource to fix the open files limit for Nginx
+exec { 'fix--for-nginx':
+  environment => ['DIR=/etc/default/nginx',         # Specify the directory of the nginx configuration file
+                  'OLD=ULIMIT="-n 15"',            # Define the old ULIMIT setting to be replaced
+                  'NEW=ULIMIT="-n 15000"'],        # Define the new ULIMIT setting
+  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo service nginx restart',  # Command to replace and restart
+  path        => ['/usr/bin', '/bin'],             # Set the search path for the command execution
+  returns     => [0, 1]                            # Specify the expected return codes (0: success, 1: no changes)
 }
